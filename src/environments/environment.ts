c653fd1f5b16442e1c30d2ab525236af98273407
nigel <nigel@webbox.co.za>
2020-08-04 19:28:51 +0200

    bulk commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCcgIFxX8VE8cONPSCvE9ylhhAaK1sT4bs",
+    authDomain: "ionic-92142.firebaseapp.com",
+    databaseURL: "https://ionic-92142.firebaseio.com",
+    projectId: "ionic-92142",
+    storageBucket: "ionic-92142.appspot.com",
+    messagingSenderId: "214578467644",
+    appId: "1:214578467644:web:2c95d6bb23051523540439",
+    measurementId: "G-3BEDB2LTV1"
+
+  }
 };
 
 /*
