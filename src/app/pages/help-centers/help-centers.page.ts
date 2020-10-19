3ae3644b8ae4509ec8104e2d8e925a414ebc14ae
nigel <nigel@webbox.co.za>
2020-10-20 01:24:35 +0200

    add icons

--- src/app/pages/help-centers/help-centers.page.ts
+++ src/app/pages/help-centers/help-centers.page.ts
@@ -12,7 +12,7 @@ export class HelpCentersPage implements OnInit {
 
   map: Leaflet.Map;
   propertyList = [];
-  
+
   constructor() { }
 
   ngOnInit() {
@@ -35,8 +35,19 @@ export class HelpCentersPage implements OnInit {
   }
 
   leafletMap() {
+    var greenIcon = L.icon({
+      iconUrl: './assets/icons/leaf-green.png',
+      shadowUrl: './assets/icons/leaf-shadow.png',
+  
+      iconSize:     [38, 95], // size of the icon
+      shadowSize:   [50, 64], // size of the shadow
+      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+      shadowAnchor: [4, 62],  // the same for the shadow
+      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+  });
+
     for (const property of this.propertyList) {
-      Leaflet.marker([property.lat, property.lng]).addTo(this.map)
+      Leaflet.marker([property.lat, property.lng], {icon: greenIcon}).addTo(this.map)
         .bindPopup(property.city)
         .openPopup();
     }
